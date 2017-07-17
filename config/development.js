933c10d77352505f1e1f443a91c7aa5958c90117
Gabriel Browarnik <gabrielbrowar@gmail.com>
2017-07-17 13:06:15 -0300

    agrego conexion a db

--- config/development.js
+++ config/development.js
@@ -3,7 +3,7 @@ exports.config = {
   isDevelopment: true,
   common: {
     database: {
-      url: process.env.NODE_API_DB_URL,
+      url: process.env.NODE_API_DB_URL || 'postgres://postgres:postgres@127.0.0.1:5432/postgres',
       host: process.env.NODE_API_DB_HOST,
       port: process.env.NODE_API_DB_PORT,
       database: process.env.NODE_API_DB_NAME,
