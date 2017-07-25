32ce9b7edba44a5aadf4c5c0a10b0d77b4f530c9
Gabriel Browarnik <gabrielbrowar@gmail.com>
2017-07-25 13:06:46 -0300

    correcting PR review

--- app/services/sessionManager.js
+++ app/services/sessionManager.js
@@ -3,8 +3,8 @@ const jwt = require('jwt-simple'),
   config = require('./../../config');
 
 const SECRET = config.common.session.secret;
-const maxUsefulDays = config.common.session.maxUsefulDays;
-const expirationDate = config.common.session.expirationDate;
+const maxUsefulDays = parseInt(config.common.session.maxUsefulDays);
+const expirationDate = parseInt(config.common.session.expirationDate);
 
 exports.HEADER_NAME = config.common.session.header_name;
 
